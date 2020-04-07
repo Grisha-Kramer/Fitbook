52afa1d57419236aa2db01c436ae69c1193a0d89
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-07 16:44:47 -0400

    finally collecting totals

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.static("public"))
 require("./routes/apiroutes")(app) 
 require("./routes/htmlroutes")(app) 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://<user>:<password1>@ds133086.mlab.com:33086/heroku_np35knxx", {
-  useNewUrlParser: true
-}) 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+})
 
 app.listen(PORT, function() {
   console.log(`Now listening on port: ${PORT}`) 
